be7ff704ecf681a025f7e7d04ceaacfb0aaa7a3f
jingwei027 <jingwei027@gmail.com>
2015-07-26 01:02:37 +0800

    fix for firebug

--- src/main/java/com/softpower/chihuahua/core/pagination/Order.java
+++ src/main/java/com/softpower/chihuahua/core/pagination/Order.java
@@ -1,15 +1,18 @@
 package com.softpower.chihuahua.core.pagination;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import com.softpower.chihuahua.core.enums.SortOption;
 
+@SuppressWarnings("serial")
 @Getter
 @Setter
 @AllArgsConstructor
-public class Order {
+public class Order implements Serializable {
 	private String name;
 	private SortOption option;
 }
--- src/main/java/com/softpower/chihuahua/core/pagination/OrderBy.java
+++ src/main/java/com/softpower/chihuahua/core/pagination/OrderBy.java
@@ -1,5 +1,6 @@
 package com.softpower.chihuahua.core.pagination;
 
+import java.io.Serializable;
 import java.util.List;
 
 import lombok.Getter;
@@ -7,7 +8,8 @@ import lombok.Getter;
 import com.google.common.collect.Lists;
 import com.softpower.chihuahua.core.enums.SortOption;
 
-public class OrderBy {
+@SuppressWarnings("serial")
+public class OrderBy implements Serializable{
 
 	@Getter
 	private List<Order> orders = Lists.newArrayList();
