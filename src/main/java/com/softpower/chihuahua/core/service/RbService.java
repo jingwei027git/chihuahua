9190c8c78e2c8581f5a75c68d6b51ac206ed2832
david cheng <david@softpower.com.tw>
2015-08-11 18:48:06 +0800

    .

--- src/main/java/com/softpower/chihuahua/core/service/RbService.java
+++
@@ -1,9 +0,0 @@
-package com.softpower.chihuahua.core.service;
-
-import java.io.Serializable;
-
-import com.softpower.chihuahua.core.entity.RbModel;
-
-public interface RbService<T extends RbModel> extends Serializable {
-
-}
