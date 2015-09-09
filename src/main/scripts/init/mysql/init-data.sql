a80e2bdd41e8ce742b899b5439cf049f10bd6049
david cheng <david@softpower.com.tw>
2015-09-09 16:10:31 +0800

    add production key

--- src/main/scripts/init/mysql/init-data.sql
+++ src/main/scripts/init/mysql/init-data.sql
@@ -12,6 +12,13 @@ VALUES (
 	1, 'SYSTEM', SYSDATE(), 'SYSTEM', SYSDATE(),
 	'Y', 'AttractionSuite_dev', '3d9a24a783494c0b9ef6eb88a811a4a5', 'http://localhost:8080/AttractionSuite'
 );
+INSERT INTO JSLOG_APP (
+	ID, CREATE_USER, CREATE_TIME, MODIFY_USER, MODIFY_TIME,
+	SYS_STATUS, NAME, APP_KEY, SITE)
+VALUES (
+	2, 'SYSTEM', SYSDATE(), 'SYSTEM', SYSDATE(),
+	'Y', 'AttractionSuite_prod', 'f3e24a542d744b2b81892de2c332816a', 'https://booking.attractionsuite.com/Attraction/'
+);
 
 
 -- ------------------------------------------------------------------
@@ -33,6 +40,11 @@ INSERT INTO JSLOG_APP_USER (
 	APP_ID, USER_ID)
 VALUES (
 	1, 1
+);
+INSERT INTO JSLOG_APP_USER (
+	APP_ID, USER_ID)
+VALUES (
+	2, 1
 );
 
 
